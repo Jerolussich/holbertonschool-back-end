ba947fb5e4747e9ac10b0d65c0ce7e32678bc0b6
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-28 07:44:56 -0300

    refactoring code due to broken checker

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""Task 1 module"""
-
-import requests
-import json
-from sys import argv
+"""
+Module 1-export_to_csv
+"""
 import csv
+import requests
+from sys import argv
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    # retrieves name
-    api_url_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-    user_list = requests.get(api_url_user).json()
-    name = user_list["name"]
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    user_name = resp.json().get('username')
 
-    # retrieves todos info from user id
-    params = {"userId": argv[1]}
-    api_url_todo = "https://jsonplaceholder.typicode.com/todos"
-    todo_list = requests.get(api_url_todo, params=params).json()
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
 
-    pending_tasks = 0
-
-    with open(f"{argv[1]}.csv", "w+") as file:
-        for item in todo_list:
+    with open(argv[1] + '.csv', 'w') as file:
+        for todo_item in resp.json():
             writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-            writer.writerow([item['userId'],
-                            name,
-                            item['completed'],
-                            item['title']])
+            writer.writerow([todo_item['userId'],
+                             user_name,
+                             todo_item['completed'],
+                             todo_item['title']])
