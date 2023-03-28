7ba8c7d397d50a47399df6477984aa032a9c923a
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-28 07:43:33 -0300

    refactorizing correct code due to broken checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-
+"""
+Module 0-gather_data_from_an_API
+"""
 import requests
 from sys import argv
 
 
-def dataGatherer():
-    # retrieves name
-    api_url_user = "https://jsonplaceholder.typicode.com/users/{}".format(
-        argv[1])
-    data_api_user = requests.get(api_url_user)
-    user_list = data_api_user.json()
-    name = user_list["name"]
+if __name__ == '__main__':
 
-    # retrieves todos info from user id
-    params = {"userId": argv[1]}
-    api_url_todo = "https://jsonplaceholder.typicode.com/todos"
-    data_api_todo = requests.get(api_url_todo, params=params)
-    todo_list = data_api_todo.json()
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+    completed = []
+    for todo_item in resp.json():
+        if todo_item['completed'] is True:
+            completed.append(todo_item['title'])
+    total_tasks = len(resp.json())
+    completed_tasks = len(completed)
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    user_name = resp.json().get('name')
 
-    pending_tasks = 0
-    for item in todo_list:
-        if item["completed"] == True:
-            pending_tasks += 1
-
-    print("Employee {} is done with tasks ({}/{}).".format(name,
-          pending_tasks, len(todo_list)))
-
-
-if __name__ == "__main__":
-    dataGatherer()
+    print('Employee {} is done with tasks({}/{}):'.format(user_name,
+                                                          completed_tasks,
+                                                          total_tasks))
+    for task in completed:
+        print('\t {}'.format(task))
