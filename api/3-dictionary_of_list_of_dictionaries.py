4cd6853a602e285141854613bea40e3be52fd2e1
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-28 07:46:56 -0300

    refactoring functinal code due to broken checker

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
-"""Task 3 module"""
-
+"""
+Module 3-dictionary_of_list_of_dictionaries
+"""
+from json import dump
 import requests
-import json
-from sys import argv
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    # retrieves name
-    api_url_user = f"https://jsonplaceholder.typicode.com/users/"
-    user_list = requests.get(api_url_user).json()
+    users_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/')
+    todos_response = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/')
 
-    # retrieves todos info from all users
-    api_url_todo = "https://jsonplaceholder.typicode.com/todos"
-    todo_list = requests.get(api_url_todo).json()
+    users = {}
+    for user in users_response.json():
+        users[user['id']] = user['username']
 
-    relevant_data_list = []
-    new_dict = {}
+    tasks = {}
+    for uid, uname in users.items():
+        for todo in todos_response.json():
+            if todo['userId'] == uid:
+                tasks.setdefault(uid, [])
+                tasks[uid].append(dict(username=uname,
+                                       task=todo['title'],
+                                       completed=todo['completed']))
 
-    # formats data tuple and prepares dict
-    for item in user_list:
-        data_tuple = (item["id"], item["username"])
-        relevant_data_list.append(data_tuple)
-
-    for item in relevant_data_list:
-        new_dict[item[0]] = []
-
-    with open(f"todo_all_employees.json", "w+") as file:
-
-        item_dict = {}
-
-        for item in todo_list:
-
-            for data_tuple in relevant_data_list:
-                if item["userId"] == data_tuple[0]:
-                    name = data_tuple[1]
-
-            item_dict["username"] = name
-            item_dict["task"] = item["title"]
-            item_dict["completed"] = item["completed"]
-
-            new_dict[item["userId"]].append(item_dict)
-
-        file.write(json.dumps(new_dict))
+    with open('todo_all_employees.json', 'w') as file:
+        dump(tasks, file)
